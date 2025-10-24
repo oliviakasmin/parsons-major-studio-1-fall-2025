162aa8f183089c42023e5efe25b6cc204c828cd7
Olivia Kasmin <oliviakasmin@gmail.com>
2025-10-24 15:56:33 -0400

    added styled Select for pension amount form

--- projects/qualitative/src/PensionAmount.tsx
+++ projects/qualitative/src/PensionAmount.tsx
@@ -1,8 +1,10 @@
 import { FunctionComponent } from 'react';
+import { PensionAmountForm } from './components/PensionAmountForm';
 export const PensionAmount: FunctionComponent = () => {
   return (
     <div>
       <h2>Pension Amount</h2>
+      <PensionAmountForm />
     </div>
   );
 };
--- projects/qualitative/src/components/PensionAmountForm.tsx
+++ projects/qualitative/src/components/PensionAmountForm.tsx
@@ -1,8 +1,67 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
+import { StyledSelect } from './StyledSelect';
 export const PensionAmountForm: FunctionComponent = () => {
+  const dateOptions = [
+    '1818',
+    '1819',
+    '1820',
+    '1821',
+    '1822',
+    '1823',
+    '1824',
+    '1825',
+    '1826',
+    '1827',
+    '1828',
+    '1829',
+    '1830',
+    '1831',
+    '1832',
+  ];
+
+  const applicantTypeOptions = ['soldier', 'widow'];
+
+  const placeOfResidenceOptions = [
+    'New York',
+    'Boston',
+    'Philadelphia',
+    'Washington',
+  ];
+
+  const defaultDate = dateOptions[0];
+  const defaultApplicantType = applicantTypeOptions[0];
+  const defaultPlaceOfResidence = placeOfResidenceOptions[0];
+
+  const [date, setDate] = useState<string>(defaultDate);
+  const [applicantType, setApplicantType] =
+    useState<string>(defaultApplicantType);
+  const [placeOfResidence, setPlaceOfResidence] = useState<string>(
+    defaultPlaceOfResidence
+  );
+
   return (
-    <div>
-      <h2>Pension Amount Form</h2>
+    <div className="pension-amount-form-container">
+      It's
+      <StyledSelect
+        value={date}
+        onChange={setDate}
+        options={dateOptions}
+        placeholder="Select a date"
+      />
+      and I am a
+      <StyledSelect
+        value={applicantType}
+        onChange={setApplicantType}
+        options={applicantTypeOptions}
+        placeholder="Select an applicant type"
+      />
+      who lives in
+      <StyledSelect
+        value={placeOfResidence}
+        onChange={setPlaceOfResidence}
+        options={placeOfResidenceOptions}
+        placeholder="Select a place of residence"
+      />
     </div>
   );
 };
