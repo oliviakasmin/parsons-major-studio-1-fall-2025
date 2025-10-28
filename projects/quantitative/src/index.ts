16f0f69a9dff582e68846576e03cf3fc9e529011
Olivia Kasmin <oliviakasmin@gmail.com>
2025-10-28 11:26:32 -0400

    rendered quantitative components as is from qualitative

---
+++ projects/quantitative/src/index.ts
@@ -0,0 +1,5 @@
+export { CategoriesChart } from './CategoriesChart';
+export { History } from './History';
+export { Intro } from './Intro';
+export { CategoriesIntro } from './CategoriesIntro';
+export { NavDownButton } from './components';
